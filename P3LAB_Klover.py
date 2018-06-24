a007b0080d4d3ad5b937c5f1ada5b2ee87019f41
klovera9755 <40437708+klovera9755@users.noreply.github.com>
2018-06-24 18:40:36 -0400

    Add files via upload
    P3Lab

---
+++ P3LAB_Klover.py
@@ -0,0 +1,25 @@
+#CTI-110
+#P3LAB1
+#Adam Klover
+#06/24/18
+A_score = 90
+B_score = 80
+C_score = 70
+D_score = 60
+F_score = 50
+   
+    
+score = int(input('Enter grade: '))
+
+if score >= A_score:
+    print('Your grade is: A')
+    
+elif score >= B_score:
+     print('Your grade is: B')
+elif score >= C_score:
+     print('Your grade is: c')
+elif score >= D_score:
+     print('Your grade is: D')
+
+elif score >= F_score:
+    print('Your grade is: F') 
